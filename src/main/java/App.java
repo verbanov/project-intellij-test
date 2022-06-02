0f69a708326b8f970713a40c7c755c2963f4992d
Slava Verbanov <v.verbanov@gmail.com>
2022-06-02 15:40:39 +0200

    add print with c

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,11 @@ public class App {
     public static void main(String[] args) {
         int a = 10;
         int b = 7;
+        int c = 0;
         System.out.println(sum(a, b));
         System.out.println(div(a, b));
+        System.out.println(sum(a, c));
+
     }
 
     public static int sum(int a, int b) {
@@ -13,4 +16,5 @@ public class App {
     public static int div(int a, int b) {
         return a - b;
     }
+
 }
